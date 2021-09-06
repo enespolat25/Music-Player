5230c048f68f54a873762c632f96562370763221
Enes Polat <37534266+enespolat25@users.noreply.github.com>
2021-09-06 12:07:08 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+let songs = [
+    {
+        name: 'Yıllar Utansın',
+        path: 'musics/Song 1.mp3',
+        artist: 'Müslüm Gürses',
+        cover: 'images/cover 1.png'
+    },
+    {
+        name: 'Nilüfer',
+        path: 'musics/Song 2.mp3',
+        artist: 'Müslüm Gürses',
+        cover: 'images/cover 2.png'
+    },
+    {
+        name: 'Senden Vazgeçmem',
+        path: 'musics/Song 3.mp3',
+        artist: 'Müslüm Gürses',
+        cover: 'images/cover 3.png'
+    },
+    {
+        name: 'Affet',
+        path: 'musics/Song 4.mp3',
+        artist: 'Müslüm Gürses',
+        cover: 'images/cover 4.png'
+    },
+    {
+        name: 'Seni Yazdım',
+        path: 'musics/Song 5.mp3',
+        artist: 'Müslüm Gürses',
+        cover: 'images/cover 5.png'
+    },
+]
